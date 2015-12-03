841b45886e3ef3397ba6e9a82277f4e1e3407b1a
Terune <sbj009@naver.com>
2015-12-03 21:53:42 +0900

    report Ver.0.2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
         @products = Product.all
+        @customers = Customer.all
+        @shops = Shop.all
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( highcharts.js )
+Rails.application.config.assets.precompile += %w( dark-unica.js )
+Rails.application.config.assets.precompile += %w( dark-green.js )
+Rails.application.config.assets.precompile += %w( dark-blue.js )
